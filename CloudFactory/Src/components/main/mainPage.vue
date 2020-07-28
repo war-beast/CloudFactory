64e70951d9a850bcd1877e3a632874df4f09a6fc
Владимир Шарапов <war-beast@yandex.ru>
2020-07-28 15:23:35 +0300

    Add project files.

---
+++ CloudFactory/Src/components/main/mainPage.vue
@@ -0,0 +1,7 @@
+﻿<template>
+	<div>Компонент главной страницы</div>
+</template>
+
+<script lang="ts">
+
+</script>
---
+++ CloudFactory/Src/pages/main/init.ts
@@ -0,0 +1,19 @@
+﻿import Vue from "vue";
+import MainPageComponent from "Components/currenciesTable/mainPage.vue";
+
+class AppCore {
+	private instance: Vue;
+
+	constructor() {
+		this.init();
+	}
+
+	private init() {
+		this.instance = new Vue({
+			el: "#vue-app-container",
+			render: (h: any) => h(MainPageComponent)
+		});
+	}
+}
+
+new AppCore();
